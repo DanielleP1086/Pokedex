ababa7551757b5d99b63b3802a01c0ead1af76e7
DanielleP1086 <petwaydanielle@gmail.com>
2020-12-10 15:54:33 +0100

    Update
    fixed error in syntax

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -2,4 +2,4 @@ let pokemonList = []
 
   let pokemon1 = {name: 'Wigglytuff', weight: 12, types: ['fairy', 'normal']}
   let pokemon2 = {name: 'Wartortle', weight: 22, types: ['monster', 'water']}
-  let pokemon3 = {name: 'Bulbasaur', weight: 7, types: ['grass', 'poison']}]
+  let pokemon3 = {name: 'Bulbasaur', weight: 7, types: ['grass', 'poison']}
