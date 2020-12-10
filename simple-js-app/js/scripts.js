50a8e8d94a90b1a46603735aea3a672fc76fb3cc
DanielleP1086 <petwaydanielle@gmail.com>
2020-12-10 15:41:37 +0100

    Pokemon List
    added Pokemon list with 3 pokemons(objects)

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
+let pokemonList = []
 
-let favoriteFood = 'seafood';
-document.write(favoriteFood);
+  let pokemon1 = {name: 'Wigglytuff', weight: 12, types: ['fairy', 'normal']}
+  let pokemon2 = {name: 'Wartortle', weight: 22, types: ['monster', 'water']}
+  let pokemon3 = {name: 'Bulbasaur', weight: 7, types: ['grass', 'poison']}]
