bc5b39510a0c8eb94bb85d189f7a7040f49768a4
DanielleP1086 <petwaydanielle@gmail.com>
2020-12-14 20:27:52 +0100

    Update scripts.js

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,6 +1,5 @@
 let pokemonList = [
-
   pokemon1 = {name: 'Wigglytuff', weight: 12, types: ['fairy', 'normal']},
   pokemon2 = {name: 'Wartortle', weight: 22, types: ['monster', 'water']},
   pokemon3 = {name: 'Bulbasaur', weight: 7, types: ['grass', 'poison']}
-]
+];
