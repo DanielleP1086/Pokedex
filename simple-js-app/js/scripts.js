8e0f4ad23257cd7f5c80198d2065eb9517924252
DanielleP1086 <petwaydanielle@gmail.com>
2020-12-16 13:26:55 +0100

    Create 'for' loop for Pokemon
    - revise syntax for array
    - create for loop
    - add conditional statement for one pokemon
    - add line break

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,5 +1,33 @@
+//let pokemonList = [
+  //{name: 'Wigglytuff', weight: 12, types: ['fairy', 'normal']},
+  //{name: 'Bulbasaur', weight: 7, types: ['grass', 'poison']},
+  //{name: 'Wartortle', weight: 22, types: ['monster', 'water']}
+//];
+
 let pokemonList = [
-  pokemon1 = {name: 'Wigglytuff', weight: 12, types: ['fairy', 'normal']},
-  pokemon2 = {name: 'Wartortle', weight: 22, types: ['monster', 'water']},
-  pokemon3 = {name: 'Bulbasaur', weight: 7, types: ['grass', 'poison']}
+  {
+    name: 'Wigglytuff',
+    weight: 12,
+    types: ['fairy', 'normal']
+  },
+  {
+    name: 'Balbasar',
+    weight: 7,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Wartortle',
+    weight: 22,
+    types: ['monster', 'water']
+  }
 ];
+
+for (let i = 0; i < pokemonList.length; i++){
+  document.write(pokemonList[i].name + " (weight:" + pokemonList[i].weight + ") ");
+  //add a conditional to highlight the biggest pokemon
+ if (pokemonList[i].weight >20){
+    document.write("-Wow! That's a big pokemon!");
+  }
+
+  document.write('<br>');
+};
