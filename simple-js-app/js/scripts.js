272612b12efa1434c1ab62884800fb945bf8e7a7
DanielleP1086 <petwaydanielle@gmail.com>
2020-12-18 07:42:36 +0100

    Add IIFE to code
    - replace 'for' loop with 'forEach' loop
    - create new pokemonRepository
    - assign pokemonRepository to IIFE
    - add public functions assigned as keys 'getAll' and 'add'

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,10 +1,5 @@
-//let pokemonList = [
-  //{name: 'Wigglytuff', weight: 12, types: ['fairy', 'normal']},
-  //{name: 'Bulbasaur', weight: 7, types: ['grass', 'poison']},
-  //{name: 'Wartortle', weight: 22, types: ['monster', 'water']}
-//];
-
-let pokemonList = [
+let pokemonRepository = (function() {
+  let pokemonList = [
   {
     name: 'Wigglytuff',
     weight: 12,
@@ -19,15 +14,29 @@ let pokemonList = [
     name: 'Wartortle',
     weight: 22,
     types: ['monster', 'water']
-  }
+  },
 ];
 
-for (let i = 0; i < pokemonList.length; i++){
-  document.write(pokemonList[i].name + " (weight:" + pokemonList[i].weight + ") ");
-  //add a conditional to highlight the biggest pokemon
- if (pokemonList[i].weight >20){
-    document.write("-Wow! That's a big pokemon!");
-  }
+function getAll() {
+  return pokemonList;
+}
 
-  document.write('<br>');
-};
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+  return {
+    getAll: getAll,
+    add: add
+    };
+})();
+
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write(pokemon.name + " (weight:" + pokemon.weight + ") ");
+//add a conditional to highlight the biggest pokemon
+if (pokemon.weight >20){
+  document.write("-Wow! That's a big pokemon!");
+}
+//add line break after each iteration
+document.write('<br>');
+});
