2ba07578777c191e0ace30af202f83af6c3acbc0
DanielleP1086 <petwaydanielle@gmail.com>
2021-01-01 14:42:35 +0100

    Add modal to Pokemon button
    - add a modal to each Pokemon button to include name, weight, and image
    - style modal container

--- simple-js-app/js/scripts.js
+++ simple-js-app/js/scripts.js
@@ -1,6 +1,8 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  //add global variable for the modalContainer
+  let modalContainer = document.querySelector('#modal-container');
 
   function add(pokemon) {
    pokemonList.push(pokemon);
@@ -22,6 +24,7 @@ function addListItem(pokemon) {
   });
 }
 
+
 //get list from API
 function loadList() {
     return fetch(apiUrl).then(function (response) {
@@ -37,7 +40,7 @@ function loadList() {
       });
     }).catch(function (e) {
       console.error(e);
-    })
+    });
   }
 
   function loadDetails(item) {
@@ -47,17 +50,71 @@ function loadList() {
       }).then(function (details) {
         // Add the details to the item
         item.imageUrl = details.sprites.front_default;
+        item.name = details.name
         item.weight = details.weight;
         item.types = details.types;
       }).catch(function (e) {
         console.error(e);
       });
     }
+
+    function showModal(item) {
+    //clear existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    //add new modal content
+    let closeButtonElement = document.createElement
+    ('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click',
+    hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = "Pokemon: " + item.name;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = "weight: " + item.weight;
+
+    let container = document.querySelector('#image-container');
+    //create an <img> elemment
+    let contentImage = document.createElement('img');
+    contentImage.src = item.imageUrl;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(contentImage);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+//add functionality to hideModal
+  function hideModal() {
+      modalContainer.classList.remove('is-visible');
+  }
+
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' &&
+    modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+
     //callback to addEventListener
-    function showDetails(item) {
-      pokemonRepository.loadDetails(item).then(function ()
+    function showDetails(pokemon) {
+      pokemonRepository.loadDetails(pokemon).then(function ()
       {
-      console.log(item);
+      showModal(pokemon);
       });
     }
 
@@ -65,12 +122,13 @@ return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    showDetails: showDetails,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails
+    showModal: showModal,
+    hideModal: hideModal
   };
 })();
-
 
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function(pokemon) {
